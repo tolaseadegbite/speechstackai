446e0728c88a78b3a2af35e48fe50a6fe356cbe4
tolaseadegbite <tolasekelvin@gmail.com>
2025-07-30 18:46:45 +0100

    Fix mobile text to speech index layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
 
   # Make these methods available as helpers in all views
   helper_method :current_user, :user_signed_in?
