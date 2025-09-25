2ae8458c87cf1a3e812c7b85dfbca9b70e79cdfe
tolaseadegbite <tolasekelvin@gmail.com>
2025-09-25 18:48:53 +0100

    Remove wavesurfer and stimulus components character counter js vendor files

--- config/importmap.rb
+++ config/importmap.rb
@@ -12,7 +12,6 @@ pin "@github/webauthn-json", to: "@github--webauthn-json.js", integrity: "sha384
 # UPDATED: Using the computed hash from your console error for @rails/request.js
 pin "@rails/request.js", to: "@rails--request.js.js", integrity: "sha384-z2lUPjZO6VEVMOJPxOpuSNtmIiFZ3ve951XEs/LRW8w/2PqkFoYIatsvFnW1xCA5" # @0.0.12
 
-pin "wavesurfer", to: "https://cdn.jsdelivr.net/npm/wavesurfer.js@7.10.1/dist/wavesurfer.esm.js"
 
 # UPDATED: Using the computed hash from your console error for lodash
 pin "lodash", to: "lodash.js", integrity: "sha384-NA8+CPLa8r4Y5V0ftYvX3rsTtKlJAhsuLcvhvsFvR+mRnliJruCdR57JoiYnKg44" # @4.17.21
